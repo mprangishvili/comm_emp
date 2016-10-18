5e6bddec52f956648cd1b29c0d68e19a5935f194
mprangishvili <12200136@ibsu.edu.ge>
2016-10-18 14:57:25 +0200

    maria - commit

--- comm_emp/src/application/RegistrationController.java
+++ comm_emp/src/application/RegistrationController.java
@@ -20,7 +20,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.SingleSelectionModel;
+//import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
